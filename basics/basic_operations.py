f1ae6d2bd369cb96c888b3242323fe44ff5120e4
Arpitawork24 <arpitasutariya2005@gmail.com>
2026-03-14 14:47:37 +0530

    subtraction

--- basics/basic_operations.py
+++ basics/basic_operations.py
@@ -1,7 +1,13 @@
 import numpy as np
 
-a = np.array([1,2,3,4])
-b = np.array([7,8,9,10])
+#addition
+a = np.array([7,8,9,10])
+b = np.array([1,2,3,4])
+res1 = np.add(a,b)
+print(res1)
 
-res = np.add(a,b)
-print(res)
+#subtraction
+c = np.array([20,35,42,67])
+d = np.array([15,7,31,59])
+res2 = np.subtract(c,d)
+print(res2)
